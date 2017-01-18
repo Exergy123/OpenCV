e171b85628613629804bb6d349438902937d0921
Daniel Casares Palomeque <exergy123@gmail.com>
2017-01-19 00:42:03 +0100

    Update example_1_2.py

--- example_1_2.py
+++ example_1_2.py
@@ -1,4 +1,4 @@
-
+###Visulizando la imagenes con Matplotlib (cuidado, no soporta BGR)
 
 
 import numpy as np
@@ -11,5 +11,5 @@ plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 plt.show()
 
 ### Color image loaded by OpenCV is in BGR mode. But Matplotlib displays in RGB mode. 
-So color images will not be displayed correctly 
-in Matplotlib if image is read with OpenCV. Please see the exercises for more details.
+### So color images will not be displayed correctly in Matplotlib if image is read with OpenCV. 
+### Please see the exercises for more details.
