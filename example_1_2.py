6217afcf721cc2c6eb1564ec32dd1619b5c4f9df
Daniel Casares Palomeque <exergy123@gmail.com>
2017-01-19 00:35:24 +0100

    Create example_1_2.py

---
+++ example_1_2.py
@@ -0,0 +1,15 @@
+
+
+
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread('messi5.jpg',0)
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.show()
+
+### Color image loaded by OpenCV is in BGR mode. But Matplotlib displays in RGB mode. 
+So color images will not be displayed correctly 
+in Matplotlib if image is read with OpenCV. Please see the exercises for more details.
